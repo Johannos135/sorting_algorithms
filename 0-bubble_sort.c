0da04f39f85ed470d175cedc08441941eacb69ef
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-10-22 00:20:09 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,18 @@
 #include "sort.h"
 
 /**
+ * more - checks whether i is greater than j
+ * @i: integer i
+ * @j: integer j
+ *
+ * Return: 1 if true otherwise 0
+ */
+int more(int i, int j)
+{
+	return (i > j);
+}
+
+/**
  * bubble_sort - function that sorts an array in
  * ascending order using bubble sort algorithm
  * @array: array to be sorted
